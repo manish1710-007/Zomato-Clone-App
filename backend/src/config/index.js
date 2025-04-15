c2160e208d98bca44a40fe65c12c9a87781cd69a
Manish <manishnik1710@gmail.com>
2025-04-16 01:18:58 +0530

    changes
    changes

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -6,4 +6,6 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,       // Load MongoDB URI from .env
     jwtSecret: process.env.JWT_SECRET,     // Load JWT secret from .env
     googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,  // Google Maps API key
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY, // Stripe secret key 
+    stripePublicKey: process.env.STRIPE_PUBLIC_KEY, // Stripe public key 
 };
