2a030936845b44b47e013b258097391771b2afdd
Manish <manishnik1710@gmail.com>
2025-05-05 01:22:58 +0530

    phew
    phew fixed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -15,4 +15,4 @@ const connectDB = async () => {
 
 module.exports = connectDB;
 
-//check & see if isn't connect, usernamechanged, pass changed, this db auth na, cluster issue, mongo updated, yeah this again 
+//finally! it fixed 
